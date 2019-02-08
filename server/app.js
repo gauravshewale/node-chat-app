5c19e8cbdfe439cb5e079fdd74531828d4f0a00f
Kumargaurav Shewale <kumargauravs@mkcl.org>
2019-02-08 18:46:36 +0530

    Basic demonstration of IO sockets

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 var clientPath = path.join(__dirname, '/../','client')
 console.log(clientPath);
 
 var app = express()
-app.use(express.static(clientPath));
+var httpServer = http.createServer(app)
+
+var io = socketIO(httpServer);
 const port = process.env.PORT || 3002;
-app.listen(port, function () {
+
+app.use(express.static(clientPath));
+
+io.on('connection', function (socket) {
+  console.log('New user connected')
+
+})
+
+httpServer.listen(port, function () {
   console.log('server is listening at:', port)  
 })
